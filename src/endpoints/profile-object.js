b41dd9cff83e4a6ba89bc41dd255076b7a6d55b5
javilobo8 <javilobo8@users.noreply.github.com>
2017-08-12 13:15:56 +0200

    fix(profile): remove console.log

--- src/endpoints/profile-object.js
+++ src/endpoints/profile-object.js
@@ -13,7 +13,6 @@ function formatProperty(prop) {
 
 class Profile {
   constructor(content) {
-    console.log(content.Stats);
     this.content = content;
     this.currentSeason = content.defaultSeason;
     this.currentSeasonLabel = content.seasonDisplay;
