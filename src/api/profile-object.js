3f5f3f2199779dd35268955ae24193ccf8c36756
javilobo8 <javilobo8@gitlab.com>
2017-08-20 00:26:26 +0200

    feat(profile): add getStatsForAllMatches and getMatchHistory methods

--- src/api/profile-object.js
+++ src/api/profile-object.js
@@ -1,5 +1,6 @@
 const {MATCH} = require('../util/constants');
 const {StatsNotFound} = require('../pubg-errors');
+const {values} = require('lodash');
 
 function formatProperty(prop) {
   const str = String(prop).replace(/\s/g, '');
@@ -12,10 +13,11 @@ class Profile {
     this.currentSeason = content.defaultSeason;
     this.currentSeasonLabel = content.seasonDisplay;
     this.lastUpdated = content.LastUpdated;
-    this.selectedRegion = content.selectedRegion;
+    this.defaultRegion = content.selectedRegion;
     this.defaultSeason = content.defaultSeason;
     this.playerName = content.PlayerName;
     this.stats = content.Stats;
+    this.matchHistory = content.MatchHistory;
 
     if (!this.stats) {
       throw new StatsNotFound();
@@ -24,7 +26,7 @@ class Profile {
 
   getStats(options = {}) {
     const {
-      region = this.selectedRegion,
+      region = this.defaultRegion,
       season = this.defaultSeason,
       match = MATCH.DEFAULT,
     } = options;
@@ -42,7 +44,9 @@ class Profile {
     let data = {};
 
     data.region = region;
+    data.defaultRegion = this.defaultRegion;
     data.season = season;
+    data.defaultSeason = this.defaultSeason;
     data.match = match;
     data.lastUpdated = this.lastUpdated;
     data.playerName = this.playerName;
@@ -62,6 +66,41 @@ class Profile {
 
       return stats;
     }, data);
+
+    return data;
+  }
+
+  getStatsForAllMatches(options = {}) {
+    const {
+      region = this.defaultRegion,
+      season = this.defaultSeason,
+    } = options;
+    const matches = values(MATCH);
+
+    const data = {};
+
+    matches.forEach((match) => {
+      try {
+        const statsOptions = Object.assign({}, {match}, {region, season});
+        data[match] = this.getStats(statsOptions);
+      } catch (err) {
+        data[match] = null;
+        console.log(`No matches for '${match}' queue`);
+      }
+    });
+
+    return data;
+  }
+
+  getMatchHistory() {
+    const data = {};
+
+    data.total = this.matchHistory.length;
+    data.matchHistory = this.matchHistory.map((entry) =>
+      Object.keys(entry).reduce((acc, curr) =>
+        Object.assign({}, acc, {[formatProperty(curr)]: entry[curr]})
+      , {})
+    );
 
     return data;
   }
