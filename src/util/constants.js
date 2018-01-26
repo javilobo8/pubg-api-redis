b94a5c1f98d2c1bb372a6f7d899651b823a28d7f
Javier Bermúdez <jbermudez@fintonic.com>
2018-01-26 21:27:50 +0100

    chore(constants): add new constants

--- src/util/constants.js
+++ src/util/constants.js
@@ -8,6 +8,7 @@ const REGION = {
   OC: 'oc',
   SA: 'sa',
   SEA: 'sea',
+  KRJP: 'krjp',
 };
 
 const SEASON = {
@@ -15,6 +16,9 @@ const SEASON = {
   EA2017pre2: '2017-pre2',
   EA2017pre3: '2017-pre3',
   EA2017pre4: '2017-pre4',
+  EA2017pre5: '2017-pre5',
+  EA2017pre6: '2017-pre6',
+  '2018-01': '2018-01',
 };
 
 const MATCH = {
